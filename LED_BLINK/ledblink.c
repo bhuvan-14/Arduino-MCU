bde74fe25ff9ec9b82dda39af4dcb201a14f6627
B Bhuvanesan <137598112+bhuvan-14@users.noreply.github.com>
2025-08-16 14:39:29 +0530

    Update ledblink.c

--- LED_BLINK/ledblink.c
+++ LED_BLINK/ledblink.c
@@ -1,1 +1,19 @@
+#define LED1 9
+#define LED2 6
+void setup() {
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+}
+void loop() {
+  digitalWrite(LED1,HIGH);
+  delay(2000);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,HIGH);
+  delay(2000);
+  digitalWrite(LED1,HIGH);
+  delay(2000);
+  digitalWrite(LED1,LOW);
+  digitalWrite(LED2,LOW);
+  delay(2000);
+}
 
